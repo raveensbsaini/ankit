ca40fbf6903c016d79bc0b14fdc8cf1775f12a9b
ravindrakumarsaini <raveensbsaini@gmail.com>
2025-02-19 17:46:06 +0530

    adding

---
+++ part3/svelte_part/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+<h1>This is layout</h1>
+{@render children()}
---
+++ part3/svelte_part/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { onMount } from "svelte";
+  
+  export let data;
+  console.log(data)
+  function on_submit(){
+    console.log("This is another function")
+  }
+  onMount(
+  function on_submit(){
+    console.log("clicked")
+  })
+  </script>
+<form on:submit = { on_submit }>
+  <h1>Please select time</h1>
+  <select>
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+    <option>5</option>
+    <option>6</option>
+    <option>7</option>
+    <option>8</option>
+    <option>9</option>
+    <option>10</option>
+    <option>11</option>
+    <option>12</option>
+  </select>
+  <select>
+    <option>am</option>
+    <option>pm</option>
+  </select>
+  <input type="submit">
+</form>
